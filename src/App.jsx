a2a0e7ece878426d36acdbba26e681ad2f8595eb
Marisa <marisavandellos@gmail.com>
2023-09-21 14:26:59 -0700

    basic setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   const [count, setCount] = useState(0)
